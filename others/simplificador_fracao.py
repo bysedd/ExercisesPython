811945f3c37078e6eefd70d9246b65a6bb60c810
Felippe <4drade@gmail.com>
2023-04-05 09:21:43 -0300

    simplificador_fracao.py

--- others/simplificador_fracao.py
+++ others/simplificador_fracao.py
@@ -9,19 +9,16 @@ try:
 
         maior = max([n1, n2])
         menor = min([n1, n2])
-        cont = (i for i in range(2, maior) if maior % i == 0 and menor % i == 0)
+        d = max((i for i in range(2, maior) if maior % i == 0 and menor % i == 0))
 
-        d = max(cont)
         n1 //= d
         n2 //= d
         r = n1 / n2
 
-        print(f'\nÉ possível simplificar essa fração por \033[1:36m{d}\033[m\n{p} = '
-              f'\033[1:36m{n1}/{n2}\033[m ou '
-              f'\033[1:36m{round(r * 100, 2)}%\033[m ou '
-              f'\033[1:36m{r:.4f}\033[m')
+        print(f'\nÉ possível simplificar essa fração por \033[1:36m{d}\033[m\n{p} ==> '
+              f'\033[1:36m{n1}/{n2}\033[m')
     else:
-        print(f'\033[31mDigite apenas números!\033[m')
+        print(f'\033[31mDigite apenas números inteiros!\033[m')
 
 except ValueError:
     print('\n\033[31mNão é possível simplificar essa fração!\033[m')
